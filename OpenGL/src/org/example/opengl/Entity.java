0ddf55b0e6f14f6b91a374efe38474625a5c64cb
t09h3r <chris.halden@gmail.com>
2012-10-28 17:40:56 +0000

    ok

---
+++ OpenGL/src/org/example/opengl/Entity.java
@@ -0,0 +1,18 @@
+package org.example.opengl;
+
+
+import javax.microedition.khronos.opengles.GL10;
+
+
+class Entity {
+  public Entity obj=new Entity();
+   public Entity() {
+   }
+   
+
+   public void draw(GL10 gl) {
+   }
+   
+   
+}
+
