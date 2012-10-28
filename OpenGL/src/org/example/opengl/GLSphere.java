c7c8844daf87390097d6849ac15e3d9a59da7075
t09h3r <chris.halden@gmail.com>
2012-10-28 23:10:44 +0000

    ok

--- OpenGL/src/org/example/opengl/GLSphere.java
+++ OpenGL/src/org/example/opengl/GLSphere.java
@@ -28,6 +28,7 @@ import android.animation.*;
 class GLSphere extends Entity{
 	private final FloatBuffer mVertexBuffer;
 	private final ShortBuffer mIndexBuffer;
+	private final NormalBuffer mNormalBuffer;
 	
 	private List<Ti> tris=new ArrayList<Ti>();
 	public GLSphere() {
@@ -48,27 +49,27 @@ class GLSphere extends Entity{
 		vertices.add(new V3(-gold,-one,0));
 		vertices.add(new V3(+gold,-one,0));
 		
-tris.add(new Ti(0,1,2));
-tris.add(new Ti(1,0,3));
-tris.add(new Ti(0,2,4));
-tris.add(new Ti(0,4,5));
-tris.add(new Ti(3,0,5));
-tris.add(new Ti(2,1,6));
-tris.add(new Ti(6,1,7));
-tris.add(new Ti(1,3,7));
-tris.add(new Ti(3,5,9));
-tris.add(new Ti(7,3,9));
-tris.add(new Ti(4,2,8));
-tris.add(new Ti(2,6,8));
-tris.add(new Ti(4,8,10));
-tris.add(new Ti(5,4,10));
-tris.add(new Ti(9,5,10));
-tris.add(new Ti(9,10,11));
-tris.add(new Ti(7,9,11));
-tris.add(new Ti(6,7,11));
-tris.add(new Ti(8,6,11));
-tris.add(new Ti(10,8,11));
-
+		tris.add(new Ti(0,1,2));
+		tris.add(new Ti(1,0,3));
+		tris.add(new Ti(0,2,4));
+		tris.add(new Ti(0,4,5));
+		tris.add(new Ti(3,0,5));
+		tris.add(new Ti(2,1,6));
+		tris.add(new Ti(6,1,7));
+		tris.add(new Ti(1,3,7));
+		tris.add(new Ti(3,5,9));
+		tris.add(new Ti(7,3,9));
+		tris.add(new Ti(4,2,8));
+		tris.add(new Ti(2,6,8));
+		tris.add(new Ti(4,8,10));
+		tris.add(new Ti(5,4,10));
+		tris.add(new Ti(9,5,10));
+		tris.add(new Ti(9,10,11));
+		tris.add(new Ti(7,9,11));
+		tris.add(new Ti(6,7,11));
+		tris.add(new Ti(8,6,11));
+		tris.add(new Ti(10,8,11));
+		
 		
 		ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.size()*4*3);
 		vbb.order(ByteOrder.nativeOrder());
@@ -103,11 +104,18 @@ tris.add(new Ti(10,8,11));
 	public void draw(GL10 gl) {
 		gl.glPushMatrix();
 		gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-	//	gl.glEnableClientState(GL10.GL_INDEN_ARRAY);
+		if (mNormalBuffer != null) {
+			// Enabled the normal buffer for writing and to be used during rendering.
+			gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
+			
+			// Specifies the location and data format of an array of normals to use when rendering.
+			gl.glNormalPointer(GL10.GL_FLOAT, 0, mNormalBuffer);
+		}
+		//	gl.glEnableClientState(GL10.GL_INDEN_ARRAY);
 		gl.glScalef(1f,1f,1f);
 		
 		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
-	//	gl.glIndexPointer(3,GL10.GL_SHORT,0,mIndexBuffer);
+		//	gl.glIndexPointer(3,GL10.GL_SHORT,0,mIndexBuffer);
 		
 		gl.glColor4f(1, 1, 1, 1);
 		gl.glNormal3f(0, 0, 1);
