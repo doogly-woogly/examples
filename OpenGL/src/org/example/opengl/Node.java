ab29857f44cd99345c496cbd4101b287a54a717c
t09h3r <chris.halden@gmail.com>
2012-10-28 14:51:53 +0000

    ok

---
+++ OpenGL/src/org/example/opengl/Node.java
@@ -0,0 +1,25 @@
+package org.example.opengl;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.opengl.GLUtils;
+
+import java.util.ArrayList;
+
+class Node {
+
+   public Node() {
+   }
+   
+
+   public void draw(GL10 gl) { 
+   }
+   
+   
+}
+
